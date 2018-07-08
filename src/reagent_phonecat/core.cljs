3c84ef32256a2c2cfac9b9f97c3daa29bf5c6aa9
romankrru <rm07ru@gmail.com>
2018-07-09 00:52:25 +0300

    User input and state

--- src/reagent_phonecat/core.cljs
+++ src/reagent_phonecat/core.cljs
@@ -1,12 +1,8 @@
 (ns reagent-phonecat.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+    (:require [reagent.core :as reagent :refer [atom]]
+              [clojure.string :as str]))
 
 (enable-console-print!)
-
-(defonce app-state (atom {:text "Hello world!"}))
-
-(declare <phones-list>
-         <phone-item>)
 
 (def hardcoded-phones-data
   [{:name "Nexus S"
@@ -14,8 +10,46 @@
    {:name "Motorola XOOM with WI-FI"
     :description "The Next, Next Generation Tablet."}])
 
+(defn update-search [state new-search]
+  (assoc state :search new-search))
 
-(defn <phone-list>
+(defn matches-search?
+  "Determines if a phone item matches a text query."
+  [search data]
+  (let [qp (-> search
+               (or "")
+               str/lower-case
+               re-pattern)]
+    (->> (vals data)
+         (filter string?)
+         (map str/lower-case)
+         (some #(re-find qp %)))))
+
+(defonce state (atom {:phones hardcoded-phones-data
+                      :search ""}))
+
+(declare <phones-list>
+         <phone-item>)
+
+(defn <search-cpnt> [search]
+  [:span
+   "Search:"
+   [:input {:type "text"
+            :value search
+            :on-change (fn [e]
+                         (swap! state
+                                update-search
+                                (-> e .-target .-value)))}]])
+
+
+(defn <top-cpnt> []
+  (let [{:keys [phones search]} @state]
+    [:div.container-fluid
+     [:div.row
+       [:div.col-4 [<search-cpnt> search]]
+       [:div.col-8 [<phones-list> phones search]]]]))
+
+(defn <phones-list>
   [phones-list]
   [:div.container-fluid
    [:ul
@@ -29,7 +63,7 @@
    [:span name]
    [:p description]])
 
-(reagent/render-component [<phone-list> hardcoded-phones-data]
+(reagent/render-component [<top-cpnt>]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload [])
